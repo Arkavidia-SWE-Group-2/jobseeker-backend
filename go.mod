d7b9dc40fa489a6043944cb305b078803807fc0a
ardhptr21 <ardhiputrapradana21@gmail.com>
2025-03-11 18:08:28 +0700

    feat(auth): change entity to use ulid instead of uuid

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/go-playground/validator/v10 v10.25.0
 	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/golang-jwt/jwt/v5 v5.2.1
-	github.com/google/uuid v1.6.0
+	github.com/oklog/ulid/v2 v2.1.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.19.0
 	golang.org/x/crypto v0.32.0
@@ -20,6 +20,7 @@ require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
--- internal/entity/user_entity.go
+++ internal/entity/user_entity.go
@@ -3,16 +3,15 @@ package entity
 import (
 	"jobseeker/internal/pkg/helper"
 
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID       uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Email    string    `json:"email" gorm:"type:varchar(255);not null;unique"`
-	Phone    string    `json:"phone" gorm:"type:varchar(255);not null;unique"`
-	Password string    `json:"password" gorm:"type:varchar(255);not null"`
-	Vanity   string    `json:"vanity" gorm:"type:varchar(255);unique;not null"`
+	BaseEntity
+	Email    string `json:"email" gorm:"type:varchar(255);not null;unique"`
+	Phone    string `json:"phone" gorm:"type:varchar(255);not null;unique"`
+	Password string `json:"password" gorm:"type:varchar(255);not null"`
+	Vanity   string `json:"vanity" gorm:"type:varchar(255);unique;not null"`
 	Timestamp
 
 	Profile *Profile `json:"profile,omitempty" gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
